6280e346a1d71e27c0a4f646b754253bff4638f9
chatblack1999 <chatblack1999@gmail.com>
2021-11-25 00:41:51 -0600

    [ADDED] booking details view

--- src/pages/BookingDetail.jsx
+++ src/pages/BookingDetail.jsx
@@ -30,16 +30,35 @@ const BookingDetail = () => {
         {/* <BookingCard /> */}
         <HeaderUser />
 
-        <div className="w-10/12 lg:w-1/2 flex flex-col m-auto bg-gray-200">
+        <div className="w-10/12 lg:w-2/3 flex flex-col m-auto bg-gray-200 p-8">
           {!loading && (
             <>
-              <div>{booking.area}</div>
-              <div></div>
-              <div></div>
-              <div></div>
+              {/* <div classNamew="w-1/2 mx-auto flex flex-col bg-gray-200 "> */}
+              <div className="mr-auto text-2xl font-semibold">
+                <span className="font-bold">Area: </span>
+                {booking.area}
+              </div>
+              <div className="mr-auto text-2xl font-semibold">
+                <span className="font-bold">Fecha: </span>
+                {booking.fecha_cita}
+              </div>
+              <div className="mr-auto text-2xl font-semibold">
+                <span className="font-bold">Hora: </span>
+                {booking.hora}
+              </div>
+              <div className="mr-auto text-2xl font-semibold">
+                <span className="font-bold">Motivo: </span>
+                {booking.motivo}
+              </div>
+              <div className="mr-auto text-2xl font-semibold">
+                <span className="font-bold">RFC: </span>
+                {booking.rfc}
+              </div>
+              {/* </div> */}
             </>
           )}
         </div>
+      <div className="w-10/12 lg:w-2/3 flex flex-col m-auto py-8 bg-blue-400 text-center  text-2xl text-white">Enlace</div>
       </div>
     </>
   );
