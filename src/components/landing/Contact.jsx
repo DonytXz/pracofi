eab5d9bc219c158cf5194eb3841a92d4638ed17b
Donato Alvarez <chatblack1999@gmail.com>
2022-02-17 14:30:23 -0600

    [FIXED] contact

--- src/components/landing/Contact.jsx
+++ src/components/landing/Contact.jsx
@@ -12,11 +12,11 @@ const Contact = () => {
       <div class="w-full h-full flex flex-col lg:flex-row">
         <div class="w-full lg:w-1/2  lg:relative p-10">
           <div class="w-full lg:w-2/3 lg:relative lg:float-right ">
-            <h2 class="my-2   font-semibold w-full text-2xl lg:text-5xl text-white text-center">
+            <h2 class="my-2   font-semibold w-full text-2xl lg:text-5xl text-white ">
             En PRACOFI nos interesa lo que piensa
             </h2>
             <p class="text-white   font-normal">
-              <strong class="font-semibold text-center" > Contacte con nosotros ahora mismo llenando este sencillo formulario </strong>
+              <strong class="font-semibold " > Contacte con nosotros ahora mismo llenando este sencillo formulario </strong>
             </p>
           </div>
         </div>
